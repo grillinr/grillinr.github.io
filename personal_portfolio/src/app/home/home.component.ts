95933e49cb1dd205b6f1b2148e249dd64548b9b2
Nathan Grilliot <grillinr@mail.uc.edu>
2025-01-19 11:24:33 -0500

    Added grid layout to about me

--- personal_portfolio/src/app/home/home.component.ts
+++ personal_portfolio/src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
 
+/**
+ * @title Card with footer
+ */
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  selector: 'about-me',
+  templateUrl: 'home.component.html',
+  styleUrl: 'home.component.scss',
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatGridListModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
